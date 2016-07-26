a83e3a1cd5acf760ba1b15ec6b5631ac60f0e574
Samuele Veneruso <samuele.veneruso@gmail.com>
2016-07-26 13:53:27 +0200

    Adde more tests

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 #Prerender User Agent S3 Cache
+
+A plugin for [Prerender](https://github.com/prerender/prerender) that cache the HTML on S3 generated based 
+on the user agent, adding the device and MD5 user agent
+
+## Usage
+
+Run the test
+
+        npm test
+        
+Install
+
+        npm install prerender-user-agent-render
+        
+Add this to your Prerender server.js:
+
+        server.use(require('prerender-user-agent-s3-cache'));
+        
+[License](LICENSE)
--- test/test.js
+++ test/test.js
@@ -59,11 +59,22 @@ describe('Prerender User Agent S3 Cache', function () {
         sandbox.restore();
     });
 
-    describe('Test before Phantom Request', function() {
+    describe('Test before Phantom Request', function () {
 
-        it('Request is not GET', function() {
-            var fakeNext = sinon.stub();
-            var res = {};
+        let fakeCacheGet = sinon.stub();
+        let fakeSetCache = sinon.stub();
+
+        beforeEach(function () {
+
+            userAgent.cache = {
+                get: fakeCacheGet,
+                set: fakeSetCache
+            };
+        });
+
+        it('Request is not GET', function () {
+            let fakeNext = sinon.stub();
+            let res = {};
 
             req.method = 'POST';
 
@@ -71,6 +82,99 @@ describe('Prerender User Agent S3 Cache', function () {
 
             assert(fakeNext.called);
         });
+
+        it('Request is a GET', function () {
+            let fakeNext = sinon.stub();
+            let res = {};
+
+            req.method = 'GET';
+
+            userAgent.beforePhantomRequest(req, res, fakeNext);
+
+            assert(fakeCacheGet.called);
+        });
     });
 
+    describe('Test after Pahtnom Request', function () {
+
+        it('The prender status code is different from 200', function () {
+            let fakeNext = sinon.stub();
+            let res = {};
+
+            req.prerender = {
+                statusCode: 400
+            };
+
+            userAgent.afterPhantomRequest(req, res, fakeNext);
+
+            assert(fakeNext.called);
+        });
+        
+    });
+
+    describe('Test S3 key generator', function () {
+
+        beforeEach(function () {
+            isMobileValue = false;
+            isTabletValue = false;
+        });
+
+        it('Mobile', function () {
+            let userAgentString = '_mobile_';
+            let md5Result = '0X0';
+            let url = 'http://test.com/page';
+
+            fakeUpdate.digest.returns(md5Result);
+            isMobileValue = true;
+
+            let s3Key = userAgent.testS3KeyGenerator.genarateKey(url, userAgentString);
+
+            assert(fakeUpdate.digest.called);
+            assert.equal(s3Key, url.concat(userAgentString).concat(md5Result));
+        });
+
+        it('Tablet', function () {
+            let userAgentString = '_tablet_';
+            let md5Result = '0X0';
+            let url = 'http://test.com/page';
+
+            fakeUpdate.digest.returns(md5Result);
+            isTabletValue = true;
+
+            let s3Key = userAgent.testS3KeyGenerator.genarateKey(url, userAgentString);
+
+            assert(fakeUpdate.digest.called);
+            assert.equal(s3Key, url.concat(userAgentString).concat(md5Result));
+        });
+
+        it('Desktop or otherwise', function () {
+            let userAgentString = '_desktop_';
+            let md5Result = '0X0';
+            let url = 'http://test.com/page';
+
+            fakeUpdate.digest.returns(md5Result);
+
+            let s3Key = userAgent.testS3KeyGenerator.genarateKey(url, userAgentString);
+
+            assert(fakeUpdate.digest.called);
+            assert.equal(s3Key, url.concat(userAgentString).concat(md5Result));
+        });
+
+        it('If is both mobile and tablet is saved as tablet', function () {
+            let userAgentString = '_tablet_';
+            let md5Result = '0X0';
+            let url = 'http://test.com/page';
+
+            isMobileValue = true;
+            isTabletValue = true;
+
+            fakeUpdate.digest.returns(md5Result);
+
+            let s3Key = userAgent.testS3KeyGenerator.genarateKey(url, userAgentString);
+
+            assert(fakeUpdate.digest.called);
+            assert.equal(s3Key, url.concat(userAgentString).concat(md5Result));
+        });
+
+    });
 });
